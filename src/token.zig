64ce5e040c40c46d9f1d168a8fb960909e7b5855
Ryoga-exe <git@ryoga.dev>
2024-06-05 19:24:04 +0900

    feat: add token.zig

---
+++ src/token.zig
@@ -0,0 +1,28 @@
+tokenType: TokenType,
+literal: []const u8,
+
+const TokenType = enum {
+    illegal,
+    eof,
+
+    // Identifier and Literal
+    ident,
+    int,
+
+    // Operator
+    assign,
+    plus,
+
+    // Delimiter
+    comma,
+    semicolon,
+
+    lparen,
+    rparen,
+    lbrace,
+    rbrace,
+
+    // Keywords
+    function,
+    let,
+};
